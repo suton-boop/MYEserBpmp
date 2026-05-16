615b8395a83d74dc2a57f6ca7b9e0b74895cf230
Suton <sutonommp@gmail.com>
2026-05-17 07:17:23 +0800

    Add favicon to public layout

--- resources/views/public/layout.blade.php
+++ resources/views/public/layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'E-Sertifikat BPMP Kaltim')</title>
 
+    <link rel="icon" href="{{ asset('favicon.ico') }}?v={{ time() }}" type="image/x-icon">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
