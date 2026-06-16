4639664cdbd97bb010cf941fe14131ee62ba50c7
Suton <sutonommp@gmail.com>
2026-06-16 17:06:21 +0800

    fix: hapus base64 css yang merusak tata letak dompdf, kembalikan ke url biasa dengan pengaturan tebal (bold) yang benar

--- resources/views/certificates/pdf.blade.php
+++ resources/views/certificates/pdf.blade.php
@@ -5,22 +5,22 @@
   <style>
     @font-face {
       font-family: 'Great Vibes';
-      src: url('data:font/truetype;charset=utf-8;base64,{{ base64_encode(file_get_contents(public_path('fonts/GreatVibes-Regular.ttf'))) }}') format('truetype');
+      src: url('{{ str_replace('\\', '/', public_path('fonts/GreatVibes-Regular.ttf')) }}') format('truetype');
       font-weight: normal;
     }
     @font-face {
       font-family: 'Great Vibes';
-      src: url('data:font/truetype;charset=utf-8;base64,{{ base64_encode(file_get_contents(public_path('fonts/GreatVibes-Regular.ttf'))) }}') format('truetype');
+      src: url('{{ str_replace('\\', '/', public_path('fonts/GreatVibes-Regular.ttf')) }}') format('truetype');
       font-weight: bold;
     }
     @font-face {
       font-family: 'Alex Brush';
-      src: url('data:font/truetype;charset=utf-8;base64,{{ base64_encode(file_get_contents(public_path('fonts/AlexBrush-Regular.ttf'))) }}') format('truetype');
+      src: url('{{ str_replace('\\', '/', public_path('fonts/AlexBrush-Regular.ttf')) }}') format('truetype');
       font-weight: normal;
     }
     @font-face {
       font-family: 'Alex Brush';
-      src: url('data:font/truetype;charset=utf-8;base64,{{ base64_encode(file_get_contents(public_path('fonts/AlexBrush-Regular.ttf'))) }}') format('truetype');
+      src: url('{{ str_replace('\\', '/', public_path('fonts/AlexBrush-Regular.ttf')) }}') format('truetype');
       font-weight: bold;
     }
     @page { size: A4 landscape; margin: 0; }
